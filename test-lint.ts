4d2b1130ad5e27b44e943801e2125973fc16098a
unknown <realassetcoin@gmail.com>
2025-10-02 10:16:44 +0300

    cleanup: remove test file

--- test-lint.ts
+++
@@ -1,9 +0,0 @@
-// Test file for pre-commit hook
-console.log("This should trigger a linting error");
-
-const testFunction = (param: any) => {
-  console.log(param);
-  return param;
-};
-
-export default testFunction;
